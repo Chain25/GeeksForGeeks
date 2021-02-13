1a5f0ca778c253fa7a23fa2e1a31720c776291f4
Chain Singh <chainrmsa3025@gmail.com>
2021-02-13 14:39:14 +0530

    Palindrome checkup of number

--- mathematics/PalindromeOrNot.java
+++ mathematics/PalindromeOrNot.java
@@ -1,10 +1,32 @@
 package mathematics;
+import java.util.Scanner;
 
 public class PalindromeOrNot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter num");
 		
+		int num = scanner.nextInt();
+		boolean bool =  checkPalindrome(num);
+		if(bool)
+			System.out.println("Palindrome");
+		else System.out.println("Not palindrome");
+				
+	}
+
+	private static boolean checkPalindrome(int num) {
+		// TODO Auto-generated method stub
+		int sum=0,val = num;
+		while(num != 0) {
+			sum = sum*10 + num%10;
+			num /= 10;
+		}
+		if(sum == val) {
+			return true;
+		}else
+		 return false;
 	}
 
 }
